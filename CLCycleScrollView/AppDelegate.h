823841cb39e6a68395eec1fae7bc9e90fce17f7b
L <L@LdeMac-mini.local>
2014-06-27 16:58:57 +0800

    Initial Commit

---
+++ CLCycleScrollView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CLCycleScrollView
+//
+//  Created by L on 14-6-27.
+//  Copyright (c) 2014年 L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CLCycleScrollViewTests/CLCycleScrollViewTests.m
@@ -0,0 +1,34 @@
+//
+//  CLCycleScrollViewTests.m
+//  CLCycleScrollViewTests
+//
+//  Created by L on 14-6-27.
+//  Copyright (c) 2014年 L. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CLCycleScrollViewTests : XCTestCase
+
+@end
+
+@implementation CLCycleScrollViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
